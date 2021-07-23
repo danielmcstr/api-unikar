ad02c476a6d8a61bca18e980af31644e9fcc0930
danielmcstr <danielcastro.97@gmail.com>
2021-07-23 17:01:05 -0300

    Create success.php

---
+++ examples/php/success.php
@@ -0,0 +1,28 @@
+
+<?php
+/**
+ * Exemplo de requisição com sucesso
+ * Subistuir $placa e $token pelos valores correspondentes.
+ */
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://unigestor.com.br/unikar/_modulos/asaas/segundaviaboleto.php',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array('placa' => $placa,'offset' => '0','limit' => '1'),
+  CURLOPT_HTTPHEADER => array(
+    "X-Token: $token",
+    'Content-Type: application/x-www-form-urlencoded'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
